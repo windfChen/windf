edf328718d52fae0e9f7b5093c5f1d7cde9365d5
windf <windf@2cf0e34f-392c-4e49-b473-d835cbf1a6fc>
2017-05-25 07:38:15 +0000

    删除WEB-INF/config配置文件 为example创建配置文件

--- windf1.3.0/src/main/java/com/windf/module/development/pojo/Module.java
+++ windf1.3.0/src/main/java/com/windf/module/development/pojo/Module.java
@@ -1,9 +1,41 @@
 package com.windf.module.development.pojo;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+import com.windf.core.constant.ModuleConstant;
+import com.windf.core.exception.EntityException;
+import com.windf.core.file.FileUtil;
+import com.windf.core.file.XmlFileUtil;
+
 public class Module {
+	/**
+	 * 加载模块
+	 * @param code 模块的code
+	 * @return
+	 * @throws EntityException
+	 */
+	public static Module loadModule(String code) throws EntityException {
+		String exampleDescriptPath = ModuleConstant.DEFAULT_MODULE_DESCRIPT_PATH + File.separator + 
+				code + File.separator  + ModuleConstant.DEFAULT_MODULE_DESCRIPT_FILE_NAME;
+		File exampleDescriptFile = FileUtil.getWebappFile(exampleDescriptPath);
+		if (!exampleDescriptFile.exists()) {
+			try {
+				throw new EntityException("模板模块：[" + code + "]的配置文件不存在");
+			} catch (EntityException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		/*
+		 *	 解析模板，获得模板配置
+		 */		
+		Module module = XmlFileUtil.readXml2Object(exampleDescriptFile, Module.class);
+		
+		return module;
+	}
+	
 	private String code;
 	private String name;
 	private String basePath;
@@ -49,6 +81,7 @@ public class Module {
 	public void setDependent(List<String> dependent) {
 		this.dependent = dependent;
 	}
+
 	
 	
 }
