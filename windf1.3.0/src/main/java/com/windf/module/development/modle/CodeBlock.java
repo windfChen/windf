1f496bfc86e7f7fdd5374725d55c03fb22d13b95
windf <windf@2cf0e34f-392c-4e49-b473-d835cbf1a6fc>
2017-06-28 09:12:25 +0000

    java 编码功能初始化和写操作

---
+++ windf1.3.0/src/main/java/com/windf/module/development/modle/CodeBlock.java
@@ -0,0 +1,28 @@
+package com.windf.module.development.modle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CodeBlock {
+
+	Comment comment;
+	List<String> codes;
+	
+	CodeBlock (List<String> codes, Comment comment) {
+		this.codes = codes;
+		this.comment = comment;
+	}
+
+	/**
+	 * write code
+	 * @return
+	 */
+	List<String> write() {
+		List<String> result = new ArrayList<String>();
+		
+		result.addAll(comment.write());
+		result.addAll(codes);
+		
+		return result;
+	}
+}
