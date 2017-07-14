4d3165902bbcbf52f58162e7c1d479dbccbe03db
windfChen <q3400p@sina.cn>
2017-07-14 15:48:21 +0800

    CollectionUtil增加map反转功能

--- src/main/java/com/windf/core/util/CollectionUtil.java
+++ src/main/java/com/windf/core/util/CollectionUtil.java
@@ -1,6 +1,8 @@
 package com.windf.core.util;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.springframework.util.CollectionUtils;
@@ -12,5 +14,26 @@ public class CollectionUtil {
 
 	public static boolean isNotEmpty(Map<? extends Object, ? extends Object> result) {
 		return result != null && result.size() > 0;
+	}
+	
+	/**
+	 * 将map进行反转，key作为value，value作为key
+	 * 如果value不具有唯一性，结果不可控
+	 * @param map
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Map reversalMap(Map map) {
+		Map<Object, Object> result = new HashMap<>();
+		
+		Iterator iterator = map.keySet().iterator();
+		while (iterator.hasNext()) {
+			Object key = (Object) iterator.next();
+			Object value = map.get(key);
+			
+			result.put(value, key);
+		}
+		
+		return map;
 	}
 }
