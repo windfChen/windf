d92e36dea3bc51f202f65e72b031894753faa861
windfChen <q3400p@sina.cn>
2017-07-14 15:46:09 +0800

    删除无用代码

--- src/main/java/com/windf/module/example/pojo/Example.java
+++ src/main/java/com/windf/module/example/pojo/Example.java
@@ -1,13 +1,4 @@
 package com.windf.module.example.pojo;
 
-import java.io.UnsupportedEncodingException;
-
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-
 public class Example {
-	public static void main(String[] args) throws UnsupportedEncodingException {
-		String cookieValueAfterDecode = new String(Base64.decode("d3d3Lnpzb2wubmV0fHwwYzg1YzZiOWQwYzExZjBmZDZhMTA3MTlhZDBjYTZlNHx8MTM5MTAwMDM3OTV8fGdqZ2I="), "utf-8");
-		String[] cookieValues = cookieValueAfterDecode.split("\\|\\|");
-		System.out.println("2:" + cookieValueAfterDecode);
-	}
 }
