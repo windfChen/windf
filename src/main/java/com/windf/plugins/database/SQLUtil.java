7620f83c74cd5cc49beeef5ca84c99b9a7ad15ed
chenyafeng <chenyafeng@whaty.com>
2017-08-17 18:46:59 +0800

    sql管理工具类

---
+++ src/main/java/com/windf/plugins/database/SQLUtil.java
@@ -0,0 +1,46 @@
+package com.windf.plugins.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLUtil {
+
+	/**
+	 * 查询sql中select和from中的字段
+	 * @param querySql
+	 * @return
+	 */
+	public static List<String> getSqlSelectNames(String querySql) {
+		querySql = querySql.toLowerCase();
+		String selectStr = querySql.substring(querySql.indexOf("select ") + "select ".length(), querySql.indexOf("from "));
+		while (selectStr.contains("(")) {
+			selectStr = selectStr.replaceAll("\\([^\\)\\(]*\\)", "");
+		}
+		String[] selects = selectStr.split(",");
+		
+		List<String> selectNames = new ArrayList<String>();
+		for (int i = 0; i < selects.length; i++) {
+			String key = null;
+
+			String select = selects[i].trim();
+			String[] ss = select.split("\\s");
+			if (ss.length == 1) {
+				ss = ss[0].split(".");
+				if (ss.length == 1) {
+					key = ss[0];
+				} else {
+					key = ss[ss.length - 1];
+				}
+			} else {
+				key = ss[ss.length - 1];
+			}
+			
+			if (key != null) {
+				selectNames.add(key.trim());
+			}
+			
+		}
+		
+		return selectNames;
+	}
+}
