53aeb65842382682c1c89ec0241a1068ca46500c
windfChen <q3400p@sina.cn>
2017-09-09 17:18:32 +0800

    sql文件整理

---
+++ document/sql/all.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `example` (
+  `id` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `nick_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
