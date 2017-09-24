d70086eae6ac8cd3c76a1ce5ea63ea37aeb9cb07
windfChen <q3400p@sina.cn>
2017-09-24 19:55:56 +0800

    后台下拉框

--- src/main/java/com/windf/module/priority/controler/RoleControler.java
+++ src/main/java/com/windf/module/priority/controler/RoleControler.java
@@ -1,9 +1,7 @@
 package com.windf.module.priority.controler;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
